28d5bcbee00f0657fd5b1d502d7f279de6c38d0a
dunya <dunyayousufzai@gamil.com>
2021-03-30 15:25:38 +0430

    updated

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+namespace Database\Seeders;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Zohra Baraki',
+            'email' => 'zohra@example.com',
+            'password' => Hash::make('zohra123'),
+        ]);
+    }
+}
